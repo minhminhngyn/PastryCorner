0c07556f81eaf5897acc41f5f31d7501ea25b897
minhminhngyn <nguyenminh26721@gmail.com>
2024-06-13 21:25:28 +0700

    homescreenfinal1

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 var swiper = new Swiper(".home-slider", {
-    spaceBetween: 30,
-    centeredSlides: true,
-    autoplay: {
-      delay: 7500,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    loop: true,
-  });
-  function myFunction(x) {
-    x.classList.toggle("change");
-  }
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 7500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  loop: true,
+});
+function myFunction(x) {
+  x.classList.toggle("change");
+}
+
