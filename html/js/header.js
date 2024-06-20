2229df16c6ba3ce36876de410151ce33a73ab3e8
minhminhngyn <nguyenminh26721@gmail.com>
2024-06-20 10:12:26 +0700

    homefinal11

--- html/js/header.js
+++ html/js/header.js
@@ -1,36 +1,52 @@
 const navMenu = document.getElementById("nav-menu"),
-      navToggle = document.getElementById("nav-toggle"),
-      navClose = document.getElementById("nav-close")
-if(navToggle){
-    navToggle.addEventListener('click', () => {
-        navMenu.classList.add('show-menu')
-    })
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
 }
-if(navClose){
-    navClose.addEventListener('click', () => {
-        navMenu.classList.remove('show-menu')
-    })
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
 }
 const navLink = document.querySelectorAll(".nav_link");
-const linkAction = () =>{
-    const navMenu = document.getElementById("nav-menu")
-    navMenu.classList.remove('show-menu')
-}
-navLink.forEach(n=>n.addEventListener('click', linkAction))
-document.addEventListener("DOMContentLoaded", function() {
-    const header = document.getElementById("header");
-    const dropdown = document.getElementById("dropdown");
-    window.addEventListener("scroll", function() {
-      if (window.scrollY > 0) {
-        header.classList.add("scrolled");
-        dropdown.classList.add("scrolled");
-      } else {
-        header.classList.remove("scrolled");
-        dropdown.classList.remove("scrolled");
-      }
-    });
+const linkAction = () => {
+  const navMenu = document.getElementById("nav-menu");
+  navMenu.classList.remove("show-menu");
+};
+navLink.forEach((n) => n.addEventListener("click", linkAction));
+document.addEventListener("DOMContentLoaded", function () {
+  const header = document.getElementById("header");
+  const dropdown = document.getElementById("dropdown");
+  window.addEventListener("scroll", function () {
+    if (window.scrollY > 0) {
+      header.classList.add("scrolled");
+      dropdown.classList.add("scrolled");
+    } else {
+      header.classList.remove("scrolled");
+      dropdown.classList.remove("scrolled");
+    }
   });
-  /*document.addEventListener("DOMContentLoaded", function() {
+});
+let searchbtn = document.querySelector(".searchbtn");
+let closebtn = document.querySelector(".closebtn");
+let searchbox = document.querySelector(".searchbox");
+
+searchbtn.onclick = function () {
+  searchbox.classList.add("active");
+  closebtn.classList.add("active");
+  searchbtn.classList.add("active");
+};
+
+closebtn.onclick = function () {
+  searchbox.classList.remove("active");
+  closebtn.classList.remove("active");
+  searchbtn.classList.remove("active");
+};
+
+/*document.addEventListener("DOMContentLoaded", function() {
     const header = document.getElementById("header");
     const loginBtn = document.getElementById("login-btn");
 
